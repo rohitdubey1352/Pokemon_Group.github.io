4273731c8ae35c71a829dd11696abc4721b765db
Rohit Dubey <56528673+rohitdubey1352@users.noreply.github.com>
2021-11-06 13:45:23 +0530

    Pokemon API
    Pokemon Group API

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { NavigationBtn } from './components/page-navigation/back-to-top';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      pokemons: [],
+      search:''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
+    .then(response => response.json())
+    .then(name => this.setState({pokemons:name.results}));
+  }
+
+  handleChange=(e) => {
+    this.setState({search: e.target.value});
+  };
+
+  render(){
+
+    const {pokemons, search } = this.state;
+    const fileteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+    return(
+      <div className="App">
+        <h1>Pokemon Group</h1>
+        <NavigationBtn/>
+        <CardList pokemons={fileteredPokemons}></CardList>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
